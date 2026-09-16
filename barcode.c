e3d126629c2938a9606a9b073209a901fae52f0f
Christopher A White <spacegraym3@users.noreply.github.com>
2026-09-16 19:46:43 +0000

    step 1 completed

--- barcode.c
+++ barcode.c
@@ -1,9 +1,16 @@
 #include "barcode.h"
 #include <stdio.h>
+#include <string.h>
 
 
 struct image* barcode(char* data, int width, int height) {
+    data = data; // This line is just to avoid unused parameter warning. You can remove it . Why is this needed?
     
-
+    struct image* img = malloc(sizeof(struct image));
+    img->width = width;
+    img->height = height;
+    img->pixels = malloc(width * height * sizeof(int)); 
+    memset(img->pixels, 0, width * height * sizeof(int));
+    return img;
 }
 
