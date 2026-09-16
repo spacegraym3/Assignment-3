aa32809bf5bd90017ee0a84ada7ab6ead5a8e2c1
Christopher A White <spacegraym3@users.noreply.github.com>
2026-09-16 19:18:30 +0000

    Added argument

--- image_calc.c
+++ image_calc.c
@@ -16,8 +16,18 @@ int main(int argc, char** argv){
         return -1;
     }
 
+    char* digits = argv[1];
+    int width = atoi(argv[2]);
+    int height = atoi(argv[3]);
+    char* output_filepath = argv[4];
 
-    // TODO: call barcode
-    
+    struct image* barcode_image = barcode(digits, width, height);
+
     // TODO: save result to file
+    int result = saveimage(output_filepath, barcode_image);
+    if(result != 0) {
+        printf("Error saving image to file: %s\n", output_filepath);
+        return -1;  
+    }
+    return result;
 }
